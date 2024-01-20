bdf9292a95ee7036953c647589ea386386ab0f62
Furkan Sarışın <Furkan Sarn@DESKTOP-DQN25QC>
2024-01-20 16:01:14 +0300

    Proje dosyası ekle.

---
+++ Uyg1Constor/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uyg1Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = new int();
+            Console.WriteLine(a);
+            bool b = new bool();
+            Console.WriteLine(b);
+
+            // Zaman z1 = new Zaman();
+            //z1.gun = 18;
+            //z1.ay = 01;
+            //z1.yil = 2024;
+            //z1.zamaniYaz();
+
+            Zaman z2 = new Zaman(18, 01, 2024);
+            z2.zamaniYaz();
+
+            Zaman z3 = new Zaman();
+            z3.zamaniYaz();
+
+            Zaman z4 = new Zaman();
+            
+        }
+
+    }
+
+    
+
+}
---
+++ Uyg1Constor/Zaman.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uyg1Constructor
+{
+    class Zaman
+    {
+        public int gun;
+        public int ay;
+        public int yil;
+        
+        public void zamaniYaz()
+        {
+            Console.WriteLine("{0}/{1}/{2}", gun, ay, yil);
+        }
+        public Zaman(int g, int a, int y)
+        {
+            gun = g;
+            ay = a;
+            yil = y;
+            Console.WriteLine("Parametreli constructor çalıştı");
+        }
+
+        public Zaman()
+        {
+            gun = DateTime.Now.Day;
+            ay = DateTime.Now.Month;
+            yil = DateTime.Now.Year;
+            Console.WriteLine("Parametresiz construcktor çalıştı");
+        }
+        static Zaman()
+        {
+            //statik const.larda erişim seviyesi tanımlanmaz(public,private)
+            //statik const. parametresiz olmaslı, parametre alamaz(int a )
+            Console.WriteLine("statik const. calisti.");
+        }
+    }
+    
+}
